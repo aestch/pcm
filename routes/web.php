bf6e5fed19fe35a0c96c8a41938ebf779b0cd3b5
mabrural <mabruralmutaqi@gmail.com>
2024-03-31 13:42:17 +0700

    pembuatan layouts main.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.index');
+    return view('layouts.main');
 });
 
 Route::get('/admin', function () {
