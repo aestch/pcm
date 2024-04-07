d5da2cc7a2ef066782145c7bd734211b165576f8
mabrural <mabruralmutaqi@gmail.com>
2024-04-07 15:56:05 +0700

    update dan delete keanggotaan sudah berhasil namun belum ada session admin yang membedakan antara guest dan admin

--- app/Http/Controllers/KeanggotaanController.php
+++ app/Http/Controllers/KeanggotaanController.php
@@ -86,4 +86,102 @@ class KeanggotaanController extends Controller
         // render view with keanggotaan
         return view('direktori.showanggota', compact('keanggotaan'));
     }
+
+    public function edit(string $id): View
+    {
+        // get keanggotaan by ID
+        $keanggotaan = Keanggotaan::findOrFail($id);
+
+        // render view with keanggotaan
+        return view('direktori.editanggota', compact('keanggotaan'));
+
+    }
+
+    public function update(Request $request,$id): RedirectResponse
+    {
+        // validate form
+        $request->validate([
+            'nama' => 'required',
+            'nbm' => 'required',
+            'tempat_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'cabang' => 'required',
+            'ranting' => 'required',
+            'alamat' => 'required',
+            'status_perkawinan' => 'required',
+            'no_hp' => 'required',
+            'pekerjaan' => 'required',
+            'foto_diri' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'file_ktam' => 'required|mimes:pdf,jpeg,jpg,png|max:2048'
+        ]);
+
+        $keanggotaan = Keanggotaan::findOrFail($id);
+
+        // check if image is uploaded
+        if($request->hasFile('foto_diri')){
+            // upload new image
+            $foto_diri = $request->file('foto_diri');
+            $foto_diri->storeAs('public/foto-diri', $foto_diri->hashName());
+
+            $file_ktam = $request->file('file_ktam');
+            $file_ktam->storeAs('public/file-ktam', $file_ktam->hashName());
+
+            // delete old image
+            Storage::delete('public/foto-diri'.$keanggotaan->foto_diri);
+            Storage::delete('public/file-ktam'.$keanggotaan->file_ktam);
+
+            // update new keanggotaan with new image
+
+            $keanggotaan->update([
+                'nama' => $request->nama,
+                'nbm' => $request->nbm,
+                'tempat_lahir' => $request->tempat_lahir,
+                'tgl_lahir' => $request->tgl_lahir,
+                'cabang' => $request->cabang,
+                'ranting' => $request->ranting,
+                'alamat' => $request->alamat,
+                'status_perkawinan' => $request->status_perkawinan,
+                'email' => $request->email,
+                'no_hp' => $request->no_hp,
+                'pekerjaan' => $request->pekerjaan,
+                'foto_diri' => $foto_diri->hashName(),
+                'file_ktam' => $file_ktam->hashName()
+            ]);
+        }else{
+
+            // update product without image
+            $keanggotaan->update([
+                'nama' => $request->nama,
+            'nbm' => $request->nbm,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tgl_lahir' => $request->tgl_lahir,
+            'cabang' => $request->cabang,
+            'ranting' => $request->ranting,
+            'alamat' => $request->alamat,
+            'status_perkawinan' => $request->status_perkawinan,
+            'email' => $request->email,
+            'no_hp' => $request->no_hp,
+            'pekerjaan' => $request->pekerjaan
+            ]);
+        }
+
+        // redirect to index
+        return redirect()->route('direktori-keanggotaan.index')->with(['success' => 'Data berhasil diubah!']);
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        // get anggota by ID
+        $keanggotaan = Keanggotaan::findOrFail($id);
+
+        // delete image
+        Storage::delete('public/foto-diri'.$keanggotaan->foto_diri);
+        Storage::delete('public/file-ktam'.$keanggotaan->file_ktam);
+
+        // delete keanggotaan
+        $keanggotaan->delete();
+
+        // redirect to index
+        return redirect()->route('direktori-keanggotaan.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
 }
