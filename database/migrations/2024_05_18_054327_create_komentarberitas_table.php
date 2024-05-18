ee2122ddd216870d3cf9eab8a2e0453d4fa702c4
mabrural <mabruralmutaqi@gmail.com>
2024-05-18 12:50:14 +0700

    pembuatan model Komentarberita dan migration komentarberita

---
+++ database/migrations/2024_05_18_054327_create_komentarberitas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('komentarberitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('komentar_berita');
+            $table->unsignedBigInteger('portalberita_id'); // Menambahkan kolom portalberita_id
+            $table->foreign('portalberita_id')->references('id')->on('portalberitas'); // Menambahkan foreign key constraint
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('komentarberitas');
+    }
+};
