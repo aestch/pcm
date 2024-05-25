67b73fd2ad8b66517fdd910d830e77d055bf5451
mabrural <mabruralmutaqi@gmail.com>
2024-05-26 01:20:55 +0700

    fitur media sosial berhasil diintegrasikan dengan admin LTE

--- resources/views/dashboard/media-sosial/create.blade.php
+++ resources/views/dashboard/media-sosial/create.blade.php
@@ -1,37 +1,78 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Buat Data Media Sosial</h1>
-</div>
-
-<div class="col-lg-8">
-    <form action="/dashboard/media-sosial" method="post" class="mb-5">
-        @csrf
-        
-        <div class="mb-3">
-            <label for="nama_medsos" class="form-label">Nama Medsos</label>
-            <input type="text" class="form-control @error('nama_medsos') is-invalid @enderror" id="nama_medsos" name="nama_medsos" value="{{ old('nama_medsos') }}" required>
-
-            @error('nama_medsos')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Media Sosial</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/dashboard/media-sosial">Media Sosial</a></li>
+              <li class="breadcrumb-item active">Tambah Data</li>
+            </ol>
+          </div>
         </div>
-        <div class="mb-3">
-            <label for="link_medsos" class="form-label">Link Medsos</label>
-            <input type="text" class="form-control @error('link_medsos') is-invalid @enderror" id="link_medsos" name="link_medsos" value="{{ old('link_medsos') }}" required>
+      </div><!-- /.container-fluid -->
+    </section>
 
-            @error('link_medsos')
-            <div class="invalid-feedback">
-                {{ $message }}
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Tambah Data</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form action="/dashboard/media-sosial" method="post">
+                @csrf
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="nama_medsos">Nama Medsos </label>
+                        <input type="text" class="form-control @error('nama_medsos') is-invalid @enderror" id="nama_medsos" name="nama_medsos" placeholder="Masukkan nama medsos" value="{{ old('nama_medsos') }}" required>
+                        
+                        @error('nama_medsos')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>                
+                    <div class="form-group">
+                        <label for="link_medsos">Link Medsos </label>
+                        <input type="text" class="form-control @error('link_medsos') is-invalid @enderror" id="link_medsos" name="link_medsos" placeholder="Masukkan link medsos" value="{{ old('link_medsos') }}" required>
+                        
+                        @error('link_medsos')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>                
+
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+              </form>
             </div>
-            @enderror
-        </div>
+            <!-- /.card -->
 
-        <button type="submit" class="btn btn-primary">Simpan</button>
-    </form>
+          </div>
+          <!--/.col (left) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
 
 @endsection
--- resources/views/dashboard/media-sosial/index.blade.php
+++ resources/views/dashboard/media-sosial/index.blade.php
@@ -1,65 +1,103 @@
 @extends('dashboard.layouts.main')
+
 @section('container')
-<link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
 
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Kontak Media Sosial</h1>
-</div>
-
-@if(session()->has('success'))
-    <div class="alert alert-success col-lg-6" role="alert">
-        {{ session('success') }}
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Media Sosial</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Media Sosial</a></li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-@endif
+    <!-- /.content-header -->
 
-<div class="table-responsive col-lg-6">
-
-    <a href="/dashboard/media-sosial/create" class="btn btn-primary btn-sm mb-3"><span data-feather="plus"></span> Tambah data</a>
-    {{-- @if($mediasosials->isEmpty())
-        <a href="/dashboard/media-sosial/create" class="btn btn-primary btn-sm mb-3"><span data-feather="plus"></span> Tambah data</a>
-    @endif --}}
-    
-    <div class="card-body table-responsive p-0">
-       
-
-    <table class="table table-striped table-sm" id="serverside">
-        <thead>
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama Media Sosial</th>
-            <th scope="col">Link Media Sosial</th>
-            <th scope="col">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        @foreach($mediasosials as $mediasosial)
-        <tr>
-            <td>{{ (($mediasosials->currentPage() - 1) * $mediasosials->perPage()) + $loop->index + 1 }}</td>
-
-            <td>{{ $mediasosial->nama_medsos }}</td>            
-            <td><a href="{{ $mediasosial->link_medsos }}" class="text-decoration-none text-hover-black" target="_blank"> <span data-feather="link"></span> Kunjungi Link</a></td>            
-            <td>
-                <a href="/dashboard/media-sosial/{{ $mediasosial->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
-                <form action="/dashboard/media-sosial/{{ $mediasosial->id }}" method="post" class="d-inline">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="badge bg-danger border-0" onclick="return confirm('Apakah anda yakin ingin menghapus?')"><span data-feather="x-circle"></span></button>
-                </form>
-            </td>
-        </tr> 
-
-
-        
-        @endforeach
-        </tbody>
-    </table>
-</div>
-
-<div class="d-flex justify-content-center">
-    {{ $mediasosials->links('pagination::bootstrap-5', ['items' => $mediasosials]) }}
-
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+  
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Media Sosial</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <a href="/dashboard/media-sosial/create" class="btn btn-primary btn-sm mb-3"><i class="fas fa-plus"></i> Tambah data</a>
+                  <table id="example1" class="table table-bordered table-striped table-hover">
+                    <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Nama Media Sosial</th>
+                      <th>Link Media Sosial</th>
+                      <th>Aksi</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($mediasosials as $mediasosial)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $mediasosial->nama_medsos }}</td>
+                            <td><a href="{{ $mediasosial->link_medsos }}" class="text-decoration-none text-hover-black" target="_blank"> <i class="fas fa-link"></i> Kunjungi Link</a></td>
+                            <td>
+                                <a href="/dashboard/media-sosial/{{ $mediasosial->id }}/edit" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                <form action="/dashboard/media-sosial/{{ $mediasosial->id }}" method="post" class="d-inline form-hapus" data-user-id="{{ $mediasosial->id }}">
+                                    @method('delete')
+                                    @csrf
+                                    <button type="submit" onclick="konfirmasiHapus(event, {{ $mediasosial->id }})" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button>
+                                </form>
+                                
+                                <script>
+                                    function konfirmasiHapus(event, userId) {
+                                        Swal.fire({
+                                            title: 'Konfirmasi',
+                                            text: 'Apakah Anda yakin ingin menghapus?',
+                                            icon: 'question',
+                                            showCancelButton: true,
+                                            confirmButtonText: 'Ya',
+                                            cancelButtonText: 'Batal'
+                                        }).then((result) => {
+                                            if (result.isConfirmed) {
+                                                // Cari form berdasarkan data-user-id
+                                                const form = document.querySelector(`.form-hapus[data-user-id='${userId}']`);
+                                                if (form) {
+                                                    form.submit();
+                                                }
+                                            }
+                                        });
+                                        // Cegah aksi default dari tombol submit
+                                        event.preventDefault();
+                                    }
+                                </script>
+                                
+                                
+                            </td>
+                        </tr> 
+                        @endforeach
+                    
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+    <!-- /.content -->
 </div>
 
 @endsection
-
