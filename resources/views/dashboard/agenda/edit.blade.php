5fd7082c2b6992be50ade56333d59d54e4c22c1c
mabrural <mabruralmutaqi@gmail.com>
2024-05-26 03:08:34 +0700

    fitur agenda sudah berhasil diintegrasikan dengan admin LTE

--- resources/views/dashboard/agenda/edit.blade.php
+++ resources/views/dashboard/agenda/edit.blade.php
@@ -1,55 +1,99 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Ubah Data Agenda</h1>
-</div>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Agenda</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/dashboard/agenda">Agenda</a></li>
+              <li class="breadcrumb-item active">Edit Data</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
-<div class="col-lg-8">
-    <form action="/dashboard/agenda/{{ $agenda->id }}" method="post" class="mb-5">
-        @method('put')
-        @csrf
-        <div class="mb-3">
-            <label for="nama_agenda" class="form-label">Nama Agenda</label>
-            <input type="text" class="form-control @error('nama_agenda') is-invalid @enderror" id="nama_agenda" name="nama_agenda" value="{{ old('nama_agenda', $agenda->nama_agenda) }}" required autofocus>
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Edit Data</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form action="/dashboard/agenda/{{ $agenda->id }}" method="post">
+                @method('put')
+                @csrf
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="nama_agenda">Nama Agenda </label>
+                        <input type="text" class="form-control @error('nama_agenda') is-invalid @enderror" id="nama_agenda" name="nama_agenda" placeholder="Masukkan nama agenda" value="{{ old('nama_agenda', $agenda->nama_agenda) }}" required>
+                        
+                        @error('nama_agenda')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="tgl_agenda">Tanggal Agenda</label>
+                        <input type="date" class="form-control @error('tgl_agenda') is-invalid @enderror" id="tgl_agenda" name="tgl_agenda" value="{{ old('tgl_agenda', $agenda->tgl_agenda) }}" required>
 
-            @error('nama_agenda')
-            <div class="invalid-feedback">
-                {{ $message }}
+                        @error('tgl_agenda')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>  
+                    <div class="form-group">
+                        <label for="start_time">Waktu Mulai</label>
+                        <input type="time" class="form-control @error('start_time') is-invalid @enderror" id="start_time" name="start_time" value="{{ old('start_time', $agenda->start_time) }}" required>
+
+                        @error('start_time')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>  
+                    <div class="form-group">
+                        <label for="end_time">Waktu Selesai</label>
+                        <input type="time" class="form-control @error('end_time') is-invalid @enderror" id="end_time" name="end_time" value="{{ old('end_time', $agenda->end_time) }}" required>
+
+                        @error('end_time')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>  
+                   
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Ubah</button>
+                </div>
+              </form>
             </div>
-            @enderror
+            <!-- /.card -->
+
+          </div>
+          <!--/.col (left) -->
         </div>
-        <div class="mb-3">
-            <label for="tgl_agenda" class="form-label">Tanggal Agenda</label>
-            <input type="date" class="form-control @error('tgl_agenda') is-invalid @enderror" id="tgl_agenda" name="tgl_agenda" value="{{ old('tgl_agenda', $agenda->tgl_agenda) }}" required>
-            @error('tgl_agenda')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="start_time" class="form-label">Waktu Mulai</label>
-            <input type="time" class="form-control @error('start_time') is-invalid @enderror" id="start_time" name="start_time" value="{{ old('start_time', $agenda->start_time) }}" required>
-            @error('start_time')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-        
-        <div class="mb-3">
-            <label for="end_time" class="form-label">Waktu Selesai</label>
-            <input type="time" class="form-control @error('end_time') is-invalid @enderror" id="end_time" name="end_time" value="{{ old('end_time', $agenda->end_time) }}" required>
-            @error('end_time')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-        </div>
-        
-        <button type="submit" class="btn btn-primary">Ubah</button>
-    </form>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
 
 @endsection
--- resources/views/dashboard/agenda/index.blade.php
+++ resources/views/dashboard/agenda/index.blade.php
@@ -1,70 +1,105 @@
 @extends('dashboard.layouts.main')
+
 @section('container')
-<link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
 
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Agenda Persyarikatan</h1>
-</div>
-
-@if(session()->has('success'))
-    <div class="alert alert-success col-lg-10" role="alert">
-        {{ session('success') }}
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Agenda</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Agenda</a></li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-@endif
+    <!-- /.content-header -->
 
-<div class="table-responsive col-lg-10">
-
-    <a href="/dashboard/agenda/create" class="btn btn-primary btn-sm mb-3"><span data-feather="plus"></span> Tambah data</a>
-
-    {{-- @if($agendas->isEmpty())
-        <a href="/dashboard/agenda/create" class="btn btn-primary btn-sm mb-3"><span data-feather="plus"></span> Tambah data</a>
-    @endif --}}
-    
-    <div class="card-body table-responsive p-0">
-       
-
-    <table class="table table-striped table-sm" id="serverside">
-        <thead>
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama Agenda</th>
-            <th scope="col">Hari / Tanggal Agenda</th>
-            <th scope="col">Waktu</th>
-            <th scope="col">Action</th>
-        </tr>
-        </thead>
-        <tbody>
-
-        @foreach($agendas as $agenda)
-        <tr>
-            <td>{{ (($agendas->currentPage() - 1) * $agendas->perPage()) + $loop->index + 1 }}</td>
-
-            <td>{{ $agenda->nama_agenda }}</td>            
-            <td>{{ \Illuminate\Support\Carbon::parse($agenda->tgl_agenda)->locale('id')->isoFormat('dddd, DD/MM/YYYY') }}</td>
-            {{-- <td>{{ \Carbon\Carbon::parse($agenda->tgl_agenda)->locale('id')->format('l, d/m/Y') }}</td>           --}}
-            <td>{{ \Carbon\Carbon::parse($agenda->start_time)->format('H:i') }} - {{ \Carbon\Carbon::parse($agenda->end_time)->format('H:i') }}</td>
-
-            <td>
-                <a href="/dashboard/agenda/{{ $agenda->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
-                <form action="/dashboard/agenda/{{ $agenda->id }}" method="post" class="d-inline">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="badge bg-danger border-0" onclick="return confirm('Apakah anda yakin ingin menghapus?')"><span data-feather="x-circle"></span></button>
-                </form>
-            </td>
-        </tr> 
-
-
-        
-        @endforeach
-        </tbody>
-    </table>
-</div>
-
-<div class="d-flex justify-content-center">
-    {{ $agendas->links('pagination::bootstrap-5', ['items' => $agendas]) }}
-
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+  
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Agenda</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <a href="/dashboard/agenda/create" class="btn btn-primary btn-sm mb-3"><i class="fas fa-plus"></i> Tambah data</a>
+                  <table id="example1" class="table table-bordered table-striped table-hover">
+                    <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Nama Agenda</th>
+                      <th>Hari / Tanggal Agenda</th>
+                      <th>Waktu</th>
+                      <th>Aksi</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($agendas as $agenda)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $agenda->nama_agenda }}</td>
+                            <td>{{ \Illuminate\Support\Carbon::parse($agenda->tgl_agenda)->locale('id')->isoFormat('dddd, DD/MM/YYYY') }}</td>
+                            <td>{{ \Carbon\Carbon::parse($agenda->start_time)->format('H:i') }} - {{ \Carbon\Carbon::parse($agenda->end_time)->format('H:i') }}</td>
+                            <td>
+                                <a href="/dashboard/agenda/{{ $agenda->id }}/edit" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                <form action="/dashboard/agenda/{{ $agenda->id }}" method="post" class="d-inline form-hapus" data-user-id="{{ $agenda->id }}">
+                                    @method('delete')
+                                    @csrf
+                                    <button type="submit" onclick="konfirmasiHapus(event, {{ $agenda->id }})" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button>
+                                </form>
+                                
+                                <script>
+                                    function konfirmasiHapus(event, userId) {
+                                        Swal.fire({
+                                            title: 'Konfirmasi',
+                                            text: 'Apakah Anda yakin ingin menghapus?',
+                                            icon: 'question',
+                                            showCancelButton: true,
+                                            confirmButtonText: 'Ya',
+                                            cancelButtonText: 'Batal'
+                                        }).then((result) => {
+                                            if (result.isConfirmed) {
+                                                // Cari form berdasarkan data-user-id
+                                                const form = document.querySelector(`.form-hapus[data-user-id='${userId}']`);
+                                                if (form) {
+                                                    form.submit();
+                                                }
+                                            }
+                                        });
+                                        // Cegah aksi default dari tombol submit
+                                        event.preventDefault();
+                                    }
+                                </script>
+                                
+                                
+                            </td>
+                        </tr> 
+                        @endforeach
+                    
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+    <!-- /.content -->
 </div>
 
 @endsection
-
