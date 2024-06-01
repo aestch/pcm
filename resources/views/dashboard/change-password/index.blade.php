7ae0289d0729ee33d0a735123cd2585483b3c4d1
mabrural <mabruralmutaqi@gmail.com>
2024-06-01 20:58:10 +0700

    perbaikan href di change-password

--- resources/views/dashboard/change-password/index.blade.php
+++ resources/views/dashboard/change-password/index.blade.php
@@ -11,7 +11,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/dashboard/pengguna-login">Ganti Kata Sandi</a></li>
+              <li class="breadcrumb-item"><a href="/dashboard/{{ $user->id }}/change-password">Ganti Kata Sandi</a></li>
             </ol>   
           </div>
         </div>
