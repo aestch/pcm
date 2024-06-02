f2e6f3b104ae2ee98a1a4cdba2510efd51570521
mabrural <mabruralmutaqi@gmail.com>
2024-06-02 15:13:54 +0700

    Pembuatan Model Setting, SettingController, dan migration. Namum migrasinya belum dijalankan

---
+++ database/migrations/2024_06_02_130642_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('favicon');
+            $table->string('nama_website');
+            $table->string('logo');
+            $table->string('carousel1');
+            $table->string('carousel2');
+            $table->string('carousel3');
+            $table->string('footer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
