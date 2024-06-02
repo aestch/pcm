41dc9e68ba1a5321b7f609614ba84dc698239387
mabrural <mabruralmutaqi@gmail.com>
2024-06-02 12:50:31 +0700

    perbaikan sidebar dengan membaut pemmbatas agar mempermudah dalam membaca menu sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -31,7 +31,7 @@
       </div>
 
       <!-- Sidebar Menu -->
-      <nav class="mt-2">
+      {{-- <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
@@ -189,7 +189,162 @@
           </li>
           @endif
         </ul>
+      </nav> --}}
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+            
+            <!-- Dashboard -->
+            <li class="nav-item">
+                <a href="/dashboard" class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+    
+            <!-- Pengelolaan Pengguna -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">PENGELOLAAN PENGGUNA</li>
+            <li class="nav-item">
+                <a href="/dashboard/pengguna-login" class="nav-link {{ Request::is('dashboard/pengguna-login*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-user-plus"></i>
+                    <p>Pengguna Login</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Identitas -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">IDENTITAS</li>
+            <li class="nav-item">
+                <a href="/dashboard/identitas-pcm" class="nav-link {{ Request::is('dashboard/identitas-pcm*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                    <p>Identitas PCM</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Galeri -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">GALERI</li>
+            <li class="nav-item">
+                <a href="/dashboard/galeri-foto" class="nav-link {{ Request::is('dashboard/galeri-foto*') ? 'active' : '' }}">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>Galeri Foto</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/galeri-video" class="nav-link {{ Request::is('dashboard/galeri-video*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-film"></i>
+                    <p>Galeri Video</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Direktori dan Arsip -->
+            @if(auth()->user()->role_id == '2')
+            <li class="nav-header text-secondary text-bold">DIREKTORI DAN ARSIP</li>
+            <li class="nav-item">
+                <a href="/dashboard/direktori-keanggotaan" class="nav-link {{ Request::is('dashboard/direktori-keanggotaan*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>Direktori Keanggotaan</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/arsip-files" class="nav-link {{ Request::is('dashboard/arsip-files*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-folder-open"></i>
+                    <p>Arsip Berkas</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/arsip-administrasi" class="nav-link {{ Request::is('dashboard/arsip-administrasi*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-file-archive"></i>
+                    <p>Arsip Administrasi</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Media Sosial -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">MEDIA SOSIAL</li>
+            <li class="nav-item">
+                <a href="/dashboard/media-sosial" class="nav-link {{ Request::is('dashboard/media-sosial*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-share-alt"></i>
+                    <p>Media Sosial</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Amal Usaha -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">AMAL USAHA</li>
+            <li class="nav-item">
+                <a href="/dashboard/amal-usaha" class="nav-link {{ Request::is('dashboard/amal-usaha*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-building"></i>
+                    <p>Amal Usaha Muhammadiyah</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Portal Berita -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">PORTAL BERITA</li>
+            <li class="nav-item">
+                <a href="/dashboard/portal-berita" class="nav-link {{ Request::is('dashboard/portal-berita*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-newspaper"></i>
+                    <p>Portal Berita</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/artikel" class="nav-link {{ Request::is('dashboard/artikel*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-book-open"></i>
+                    <p>Artikel</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/agenda" class="nav-link {{ Request::is('dashboard/agenda*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-calendar-alt"></i>
+                    <p>Agenda</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/pengumuman" class="nav-link {{ Request::is('dashboard/pengumuman*') ? 'active' : '' }}">
+                    <i class="nav-icon far fa-bell"></i>
+                    <p>Pengumuman</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/dashboard/ortom" class="nav-link {{ Request::is('dashboard/ortom*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-network-wired"></i>
+                    <p>Ortom</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Keuangan dan Kas -->
+            @if(auth()->user()->role_id == '3')
+            <li class="nav-header text-secondary text-bold">KEUANGAN DAN KAS</li>
+            <li class="nav-item">
+                <a href="/dashboard/kas" class="nav-link {{ Request::is('dashboard/kas*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-credit-card"></i>
+                    <p>Keuangan dan Kas</p>
+                </a>
+            </li>
+            @endif
+    
+            <!-- Kajian -->
+            @if(auth()->user()->role_id == '1')
+            <li class="nav-header text-secondary text-bold">KAJIAN</li>
+            <li class="nav-item">
+                <a href="/dashboard/kajian" class="nav-link {{ Request::is('dashboard/kajian*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-lightbulb"></i>
+                    <p>Kajian</p>
+                </a>
+            </li>
+            @endif
+        </ul>
       </nav>
+    
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
