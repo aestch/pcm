a7f953c11d337a2f62571b8ef2904aeb26c29e87
mabrural <mabruralmutaqi@gmail.com>
2024-06-06 22:00:20 +0700

    insert, update, dan delete carousel sudah berhasil

--- app/Http/Controllers/CarouselController.php
+++ app/Http/Controllers/CarouselController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carousel;
+use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 
 class CarouselController extends Controller
 {
@@ -12,7 +15,10 @@ class CarouselController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.carousel.index', [
+            'carousels' => Carousel::latest()->paginate(12),
+            'pengaturan' => Setting::first()
+        ]);
     }
 
     /**
@@ -20,15 +26,34 @@ class CarouselController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.carousel.create', [
+            'pengaturan' => Setting::first()
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        // validate form
+        $request->validate([
+            'judul'=> 'required|max:80',
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/carousel', $image->hashName());
+
+        // create galerifoto
+        Carousel::create([
+            'judul' => $request->input('judul'),
+            'image' => $image->hashName(),
+        ]);
+
+        // redirect to index
+        return redirect()->route('carousel.index')->with(['success' => 'Data berhasil disimpan!']);
     }
 
     /**
@@ -42,24 +67,70 @@ class CarouselController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Carousel $carousel)
+    public function edit($id)
     {
-        //
+        $carousel = Carousel::findOrFail($id);
+        return view('dashboard.carousel.edit', [
+            'carousel' => $carousel,
+            'pengaturan' => Setting::first()
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Carousel $carousel)
+    public function update(Request $request, $id)
     {
-        //
+        $carousel = Carousel::findOrFail($id);
+     
+         $rules = [
+             'judul'=> 'required|max:80',
+             'image' => 'image|max:2048', // Validasi untuk gambar
+         ];
+     
+         $request->validate($rules);
+     
+         // Jika terdapat file gambar yang diunggah
+         if ($request->hasFile('image')) {
+             // Hapus gambar lama jika ada
+             if ($carousel->image) {
+                 Storage::delete('public/carousel/' . $carousel->image); // Ubah path penyimpanan gambar lama
+             }
+     
+             // Simpan gambar yang diunggah ke direktori 'public'
+             $imagePath = $request->file('image')->store('public/carousel');
+     
+             // Ambil nama file tanpa direktori
+             $imageName = basename($imagePath);
+     
+             // Simpan nama file ke dalam database
+             $carousel->image = $imageName;
+         }
+     
+         // Update judul
+         $carousel->judul = $request->judul;
+     
+         // Simpan perubahan pada data carousel
+         $carousel->save();
+     
+         return redirect('/dashboard/carousel')->with('success', 'Data berhasil diupdate!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Carousel $carousel)
+    public function destroy($id)
     {
-        //
+        //get foto by ID
+        $carousel = Carousel::findOrFail($id);
+
+        //delete image
+        Storage::delete('public/carousel/'. $carousel->image);
+
+        //delete product
+        $carousel->delete();
+
+        //redirect to index
+        return redirect()->route('carousel.index')->with(['success' => 'Data Berhasil Dihapus!']);
     }
 }
---
+++ resources/views/dashboard/carousel/create.blade.php
@@ -0,0 +1,106 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Carousel</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/dashboard/carousel">Carousel</a></li>
+              <li class="breadcrumb-item active">Tambah Data</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary ">
+              <div class="card-header">
+                <h3 class="card-title">Tambah Data</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form action="/dashboard/carousel" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="card-body">
+
+                    <div class="mb-3">
+                        <label for="judul" class="form-label">Judul</label>
+                        <input type="text" class="form-control @error('judul') is-invalid @enderror" id="judul" name="judul" value="{{ old('judul') }}" placeholder="Judul" required>
+                        @error('judul')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Gambar</label>
+                        <div class="input-group">
+                            <input type="file" class="form-control d-none" id="image" name="image" onchange="previewImage()" required>
+                            <label for="image" class="custom-file-upload btn btn-primary">
+                                <i class="fas fa-upload"></i> Pilih Gambar
+                            </label>
+                            <span class="file-name"></span>
+                        </div>
+                        <img class="img-preview img-fluid mt-2 col-sm-5" style="display: none;">
+                        @error('image')
+                        <div class="invalid-feedback" style="display: block;">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Upload</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+            <br><br>
+
+          </div>
+          <!--/.col (left) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+@endsection
+
+<script>
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        // Cek apakah ada file yang dipilih
+        if (image.files && image.files[0]) {
+            const oFReader = new FileReader();
+
+            oFReader.onload = function (oFREvent) {
+                // Menampilkan gambar yang dipilih ke dalam img-preview
+                imgPreview.style.display = 'block';
+                imgPreview.src = oFREvent.target.result;
+            };
+
+            // Membaca file gambar yang dipilih
+            oFReader.readAsDataURL(image.files[0]);
+        }
+    }
+</script>
