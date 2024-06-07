c833f92270a306e1a1c4c1249b6172a8d9c0cca7
mabrural <mabruralmutaqi@gmail.com>
2024-06-07 21:15:46 +0700

    perbaikan gambar di kata sambutan

--- resources/views/katasambutan.blade.php
+++ resources/views/katasambutan.blade.php
@@ -7,9 +7,16 @@
         <div class="col-lg-10 mt-5">
             <h1 class="text-center">Kata Sambutan Ketua PCM</h1><hr>
             <br>
+            <style>
+                .card-img-top {
+                  width: 100%; /* memastikan gambar selebar card */
+                  height: 500px; /* menetapkan tinggi gambar */
+                  object-fit: cover; /* memastikan gambar menutupi card dengan proporsi yang benar */
+                }
+              </style>
             <div class="container">
                 @foreach($direktorikeanggotaans as $anggota)
-                    <center><img src="{{ asset('storage/foto-diri/'. $anggota->foto_diri) }}" alt="" class="img-fluid mb-5"></center>
+                    <center><img src="{{ asset('storage/foto-diri/'. $anggota->foto_diri) }}" alt="" class="card-img-top img-fluid mb-5"></center>
                 @endforeach
 
                 <div class="row">
